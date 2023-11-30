554ed84b7f9fd9d788b1a0999779877a5dfe38c4
Amul24 <aroraamul24@gmail.com>
2023-11-30 21:59:24 +0530

    Portfolio

---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,28 @@
+export default function HeroSection() {
+  return (
+    <section id="heroSection" className="hero--section">
+      <div className="hero--section--content--box">
+        <div className="hero--section-content">
+          <p className="section--title">Hey, I'm Amul</p>
+          <h1 className="hero--section--title">
+            <span className="hero--section-title--color">Full Stack</span>{" "}
+            <br />
+            Developer
+          </h1>
+          <p className="hero--section-description">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat,
+            dignissimos omnis. Doloremque quam, consectetur quo ratione eius
+            nemo, laudantium, vel saepe cupiditate obcaecati ad expedita hic
+            placeat eaque nulla praesentium? <br />
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem
+            odit facilis consequatur molestiae voluptas consequuntur!
+          </p>
+        </div>
+        <button className="btn btn-primary">Get In Touch</button>
+      </div>
+      <div className="hero--section--img">
+        <img src="./img/hero_img.png" alt="Hero Section" />
+      </div>
+    </section>
+  );
+}
